4c8a368d0986da282abc900bd72f8d12f9cdbc43
Theo Higgins <theo.higgins@Theos-MacBook-Pro.local>
2022-08-23 19:58:01 -0400

    Create class for each function.

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package converter;
 
-import java.util.*;
-import java.lang.StringBuilder;
+
+import static converter.Converter.convert;
+import static converter.Input.getDecimalNumber;
+import static converter.Input.getTargetBase;
 
 public class Main {
 
@@ -11,57 +13,6 @@ public class Main {
         int base = getTargetBase();
         String result = convert(number, base).toString();
         System.out.println("Conversion result: " + result);
-    }
-
-    public static int getDecimalNumber() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter number in decimal system: ");
-        int input = 0;
-        try {
-            input = in.nextInt();
-            in.close();
-        } catch (Exception e) {
-            getDecimalNumber();
-        }
-        return input;
-    }
-
-
-    public static int getTargetBase(){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter target base: ");
-        int base = in.nextInt();
-        in.close();
-        if(base != 2 && base != 8 && base != 16){
-            getTargetBase();
-        }
-        return base;
-        }
-
-
-    public static StringBuilder convert (int number, int base) {
-        Hex hex = new Hex();
-
-        StringBuilder output = new StringBuilder();
-        ArrayList<Object> remainders = new ArrayList<>();
-        double quotient = number;
-       // double remainder = Math.floor(quotient % base);
-        do{
-            remainders.add((int)Math.floor(quotient % base));
-            quotient = Math.floor(quotient / base);
-           // remainder = Math.floor(quotient % base);
-        }while (quotient >= 1);
-
-        Collections.reverse(remainders);
-
-        for (Object o : remainders) {
-            if (base == 16 && (int)o  > 9){
-                output.append(hex.get((int)o));
-            } else {output.append(o);}
-
-        }
-
-        return output;
     }
 }
 
